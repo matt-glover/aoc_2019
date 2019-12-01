0217b55150501fe8fa0f54d0a2a27cabeeb8f1ae
Matt Glover <mpglover@gmail.com>
2019-12-01 13:16:29 -0500

    Recursively calculate the total fuel for a mass

--- day1/fuel_count/lib/fuel_count/launch_fuel.ex
+++ day1/fuel_count/lib/fuel_count/launch_fuel.ex
@@ -12,7 +12,20 @@ defmodule FuelCount.LaunchFuel do
     1
 
   """
-  def fuel_for_mass(mass) do
-    floor(mass / 3) - 2
+  def fuel_for_mass(component_mass) do
+    total_fuel_for_mass(0, calc_fuel(component_mass))
+  end
+
+  defp total_fuel_for_mass(current_total, fuel_mass) when fuel_mass < 1 do
+    current_total
+  end
+
+  defp total_fuel_for_mass(current_total, fuel_mass) do
+    fuel_total = calc_fuel(fuel_mass)
+    total_fuel_for_mass(current_total + fuel_mass, fuel_total)
+  end
+
+  defp calc_fuel(value) do
+    floor(value / 3) - 2
   end
 end
