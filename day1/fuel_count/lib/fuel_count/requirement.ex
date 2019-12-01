11f44434767a42a0c4d0b3241e26aedb651bea3f
Matt Glover <mpglover@gmail.com>
2019-12-01 12:59:42 -0500

    Fuel aggregator impl

--- day1/fuel_count/lib/fuel_count/requirement.ex
+++ day1/fuel_count/lib/fuel_count/requirement.ex
@@ -7,5 +7,10 @@ defmodule FuelCount.Requirement do
   Aggregate the complete fuel requirements for a ship based on the module masses captured in the provided file
   """
   def aggregate(module_mass_path) do
+    File.stream!(module_mass_path)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.map(&FuelCount.LaunchFuel.fuel_for_mass/1)
+    |> Enum.sum()
   end
 end
