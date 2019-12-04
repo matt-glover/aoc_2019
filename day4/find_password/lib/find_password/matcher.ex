36fb6d5a480bbfe05390467c3d78144d8124a63a
Matt Glover <mpglover@gmail.com>
2019-12-04 18:39:09 -0500

    Implement password match constraints

--- day4/find_password/lib/find_password/matcher.ex
+++ day4/find_password/lib/find_password/matcher.ex
@@ -18,18 +18,32 @@ defmodule FindPassword.Matcher do
   end
 
   def is_match?(candidate) do
-    #       However, they do remember a few key facts about the password:
+    cond do
+      candidate < 100_000 -> false
+      candidate > 999_999 -> false
+      digits_decrease?(candidate) -> false
+      no_digit_pairs?(candidate) -> false
+      true -> true
+    end
+  end
 
-    #     It is a six-digit number.
-    #     The value is within the range given in your puzzle input.
-    #     Two adjacent digits are the same (like 22 in 122345).
-    #     Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+  defp digits_decrease?(value) do
+    original = Integer.digits(value)
 
-    # Other than the range rule, the following are true:
+    comparison =
+      original
+      |> Enum.sort()
 
-    #     111111 meets these criteria (double 11, never decreases).
-    #     223450 does not meet these criteria (decreasing pair of digits 50).
-    #     123789 does not meet these criteria (no double).
-    false
+    original != comparison
+  end
+
+  defp no_digit_pairs?(value) do
+    comparison =
+      value
+      |> Integer.digits()
+      |> Enum.dedup()
+      |> Integer.undigits()
+
+    value == comparison
   end
 end
