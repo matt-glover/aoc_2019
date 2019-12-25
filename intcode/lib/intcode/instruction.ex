6a2d061ac7ef55f53fa2f1eae711695ac6378784
Matt Glover <mpglover@gmail.com>
2019-12-25 17:49:09 -0500

    Implement new day 5 part 2 instructions

--- intcode/lib/intcode/instruction.ex
+++ intcode/lib/intcode/instruction.ex
@@ -21,6 +21,22 @@ defmodule Intcode.Instruction do
     %Instruction{op_code: :output, parameters: parameters}
   end
 
+  def new(:jump_if_true, parameters = {_compare_input, _new_pointer}) do
+    %Instruction{op_code: :jump_if_true, parameters: parameters}
+  end
+
+  def new(:jump_if_false, parameters = {_compare_input, _new_pointer}) do
+    %Instruction{op_code: :jump_if_false, parameters: parameters}
+  end
+
+  def new(:less_than, parameters = {_input_1, _input_2, _output_location}) do
+    %Instruction{op_code: :less_than, parameters: parameters}
+  end
+
+  def new(:equals, parameters = {_input_1, _input_2, _output_location}) do
+    %Instruction{op_code: :equals, parameters: parameters}
+  end
+
   def new(:halt, {}) do
     %Instruction{op_code: :halt, parameters: {}}
   end
@@ -59,6 +75,56 @@ defmodule Intcode.Instruction do
   def apply_instruction(program, %Instruction{op_code: :output, parameters: {value}}) do
     IO.puts("Output: #{value}")
     %{program | instruction_pointer: program.instruction_pointer + 2}
+  end
+
+  def apply_instruction(program, %Instruction{
+        op_code: :jump_if_true,
+        parameters: {compare_input, new_pointer}
+      }) do
+    if compare_input == 0 do
+      %{program | instruction_pointer: program.instruction_pointer + 2}
+    else
+      %{program | instruction_pointer: new_pointer}
+    end
+  end
+
+  def apply_instruction(program, %Instruction{
+        op_code: :jump_if_false,
+        parameters: {compare_input, new_pointer}
+      }) do
+    if compare_input == 0 do
+      %{program | instruction_pointer: new_pointer}
+    else
+      %{program | instruction_pointer: program.instruction_pointer + 2}
+    end
+  end
+
+  def apply_instruction(program, %Instruction{
+        op_code: :less_than,
+        parameters: {input_1, input_2, destination}
+      }) do
+    memory =
+      if input_1 < input_2 do
+        Intcode.Memory.write_memory(program.memory, destination, 1)
+      else
+        Intcode.Memory.write_memory(program.memory, destination, 0)
+      end
+
+    %Intcode.Program{memory: memory, instruction_pointer: program.instruction_pointer + 4}
+  end
+
+  def apply_instruction(program, %Instruction{
+        op_code: :equals,
+        parameters: {input_1, input_2, destination}
+      }) do
+    memory =
+      if input_1 == input_2 do
+        Intcode.Memory.write_memory(program.memory, destination, 1)
+      else
+        Intcode.Memory.write_memory(program.memory, destination, 0)
+      end
+
+    %Intcode.Program{memory: memory, instruction_pointer: program.instruction_pointer + 4}
   end
 
   def apply_instruction(program, %Instruction{op_code: :halt}) do
--- intcode/test/intcode/instruction_test.exs
+++ intcode/test/intcode/instruction_test.exs
@@ -10,11 +10,31 @@ defmodule Intcode.InstructionTest do
     assert("1,2,3,5,99" == Intcode.Memory.dump(updated_memory))
   end
 
+  test "apply_instruction/2 for :add moves the pointer by 4" do
+    program = Intcode.Program.new("1,2,3,3,99")
+    instruction = %Instruction{op_code: :add, parameters: {2, 3, 3}}
+
+    %Intcode.Program{instruction_pointer: pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(4 == pointer)
+  end
+
   test "apply_instruction/2 for :multiply multiplies two locations and updates the target" do
     program = Intcode.Program.new("2,2,3,3,99")
     instruction = %Instruction{op_code: :multiply, parameters: {2, 3, 3}}
     %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
     assert("2,2,3,6,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :multiply moves the pointer by 4" do
+    program = Intcode.Program.new("2,2,3,3,99")
+    instruction = %Instruction{op_code: :multiply, parameters: {2, 3, 3}}
+
+    %Intcode.Program{instruction_pointer: pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(4 == pointer)
   end
 
   test "apply_instruction/2 for :input sets the location to user input" do
@@ -45,6 +65,20 @@ defmodule Intcode.InstructionTest do
     assert_received {:block_result, "3,9876,99"}
   end
 
+  test "apply_instruction/2 for :input moves the pointer 2" do
+    program = Intcode.Program.new("3,1,99")
+    instruction = %Instruction{op_code: :input, parameters: {1}}
+
+    capture_io([input: "9876\n", capture_prompt: false], fn ->
+      %Intcode.Program{instruction_pointer: updated_pointer} =
+        Instruction.apply_instruction(program, instruction)
+
+      send(self(), {:block_result, updated_pointer})
+    end)
+
+    assert_received {:block_result, 2}
+  end
+
   test "apply_instruction/2 for :output prints out the value" do
     program = Intcode.Program.new("4,0,99")
     instruction = %Instruction{op_code: :output, parameters: {4}}
@@ -63,9 +97,121 @@ defmodule Intcode.InstructionTest do
     end)
   end
 
+  test "apply_instruction/2 for :output moves the pointer by 2" do
+    program = Intcode.Program.new("4,0,99")
+    instruction = %Instruction{op_code: :output, parameters: {4}}
+
+    capture_io(fn ->
+      result = Instruction.apply_instruction(program, instruction)
+      assert 2 == result.instruction_pointer
+    end)
+  end
+
   test "apply_instruction/2 for :halt returns unmodified program" do
     program = Intcode.Program.new("1,2,1,1,99", 4)
     instruction = %Instruction{op_code: :halt, parameters: {}}
     assert program == Instruction.apply_instruction(program, instruction)
+  end
+
+  test "apply_instruction/2 for :jump_if_true sets the instruction pointer for a non-zero parameter" do
+    program = Intcode.Program.new("4,-1,4,1,7,3,4,99")
+    instruction = %Instruction{op_code: :jump_if_true, parameters: {-1, 7}}
+
+    %Intcode.Program{instruction_pointer: pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(7 == pointer)
+  end
+
+  test "apply_instruction/2 for :jump_if_true moves past the instruction for a zero parameter" do
+    program = Intcode.Program.new("4,0,4,1,7,3,4,99")
+    instruction = %Instruction{op_code: :jump_if_true, parameters: {0, 7}}
+
+    %Intcode.Program{instruction_pointer: pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(2 == pointer)
+  end
+
+  test "apply_instruction/2 for :jump_if_false sets the instruction pointer for a zero parameter" do
+    program = Intcode.Program.new("5,0,4,1,7,3,4,99")
+    instruction = %Instruction{op_code: :jump_if_false, parameters: {0, 7}}
+
+    %Intcode.Program{instruction_pointer: pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(7 == pointer)
+  end
+
+  test "apply_instruction/2 for :jump_if_false moves past the instruction for a non-zero parameter" do
+    program = Intcode.Program.new("5,-1,4,1,7,3,4,99")
+    instruction = %Instruction{op_code: :jump_if_false, parameters: {-1, 7}}
+
+    %Intcode.Program{instruction_pointer: pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(2 == pointer)
+  end
+
+  test "apply_instruction/2 for :less_than stores 1 if first param is less than second" do
+    program = Intcode.Program.new("7,2,3,3,99")
+    instruction = %Instruction{op_code: :less_than, parameters: {2, 3, 3}}
+    %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
+    assert("7,2,3,1,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :less_than stores 0 if first param is greater than second" do
+    program = Intcode.Program.new("7,4,3,3,99")
+    instruction = %Instruction{op_code: :less_than, parameters: {4, 3, 3}}
+    %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
+    assert("7,4,3,0,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :less_than stores 0 if first param is equal to second" do
+    program = Intcode.Program.new("7,2,2,3,99")
+    instruction = %Instruction{op_code: :less_than, parameters: {2, 2, 3}}
+    %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
+    assert("7,2,2,0,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :less_than increments the pointer by 4" do
+    program = Intcode.Program.new("7,2,2,3,99")
+    instruction = %Instruction{op_code: :less_than, parameters: {2, 2, 3}}
+
+    %Intcode.Program{instruction_pointer: updated_pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(updated_pointer == 4)
+  end
+
+  test "apply_instruction/2 for :equals stores 0 if first param is less than second" do
+    program = Intcode.Program.new("8,2,3,3,99")
+    instruction = %Instruction{op_code: :equals, parameters: {2, 3, 3}}
+    %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
+    assert("8,2,3,0,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :equals stores 0 if first param is greater than second" do
+    program = Intcode.Program.new("8,3,2,3,99")
+    instruction = %Instruction{op_code: :equals, parameters: {3, 2, 3}}
+    %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
+    assert("8,3,2,0,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :equals stores 1 if first param is equal to second" do
+    program = Intcode.Program.new("8,2,2,3,99")
+    instruction = %Instruction{op_code: :equals, parameters: {2, 2, 3}}
+    %Intcode.Program{memory: updated_memory} = Instruction.apply_instruction(program, instruction)
+    assert("8,2,2,1,99" == Intcode.Memory.dump(updated_memory))
+  end
+
+  test "apply_instruction/2 for :equals increments the pointer by 4" do
+    program = Intcode.Program.new("8,2,2,3,99")
+    instruction = %Instruction{op_code: :equals, parameters: {2, 2, 3}}
+
+    %Intcode.Program{instruction_pointer: updated_pointer} =
+      Instruction.apply_instruction(program, instruction)
+
+    assert(updated_pointer == 4)
   end
 end
